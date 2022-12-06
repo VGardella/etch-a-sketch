86b7f7cc09de387648d743b86ec36c98a1f3136e
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-12-06 14:46:47 -0300

    Add function and button listeners

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,9 +5,13 @@ const overlap = document.getElementById('overlap');
 const fade = document.getElementById('fade');
 
 let grid = 0;
+let row = 0;
+
+
+// board creation
 
 function gridSize(event) {
-    let row = Number(prompt('Choose the size:'));
+    row = Number(prompt('Choose the size:'));
     if (row > 100) {
         alert('Choose a number between 1 and 100!');
     }
@@ -29,6 +33,24 @@ function gridSize(event) {
 function deleteGrid(event) {
     grid.forEach((cell) => board.removeChild(cell));
 }
+
+function createBoard(event) {
+    if (grid === 0) {
+        gridSize();
+    }
+    else if (grid !== 0) {
+        deleteGrid();
+        gridSize();
+    }
+}
+
+function resetBoard(event) {
+    grid.forEach((cell) => {
+        cell.style.cssText = 'background-color: transparent;';
+    })
+}
+
+// drawing
 
 function colorChange(event) {
     grid.forEach((cell) => cell.addEventListener('mouseover', function() {
@@ -53,16 +75,47 @@ function colorFade(event) {
     }))
 }
 
-let createBoard = function(event) {
-    if (grid === 0) {
-        gridSize();
-    }
-    else if (grid !== 0) {
-        deleteGrid();
-        gridSize();
-    }
-    colorChange();
-    increaseOpacity();
+// reset drawing functions
+
+function resetFade(event) {
+    grid.forEach((cell) => cell.removeEventListener('mouseout', function() {
+        this.classList.remove('fade');
+        void this.offsetWidth; // requesting element dimensions cause reflow and restarts the animation.
+        this.classList.add('fade');
+    }))
 }
 
-size.addEventListener('click', createBoard);
+function resetOpacity(event) {
+    grid.forEach((cell) => cell.removeEventListener('mouseover', function() {
+        let style = getComputedStyle(this);
+        let opacity = parseFloat(style.opacity);
+        let newOpacity = opacity + 0.2;
+        this.style.opacity = newOpacity;
+    }))
+    grid.forEach((cell) => cell.removeEventListener('mouseover', function() {
+        this.classList.add('color');
+    }))
+}
+
+
+// button activation
+
+size.addEventListener('click', () => {
+    createBoard();
+});
+
+overlap.addEventListener('click', () => {
+    resetBoard();
+    colorChange();
+    increaseOpacity();
+})
+
+reset.addEventListener('click', () => {
+    resetBoard();
+})
+
+fade.addEventListener('click', () => {
+    resetBoard();
+    resetOpacity();
+    colorFade();
+})
