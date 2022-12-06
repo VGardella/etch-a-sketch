0df05292c27f832d4657bba00459c37e3d672a66
Victoria Gardella <v.gardella.ruiz@gmail.com>
2022-12-05 23:10:01 -0300

    Add row input exceptions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,12 +3,20 @@ const button = document.getElementById('btn');
 
 let gridSize = function (event) {
     let row = Number(prompt('Choose the size:'));
-    for (let i = 0; i < row*row; i++) {
+    if (row > 70) {
+        alert('Choose a number between 1 and 70!');
+    }
+    else if (row <= 0) {
+        alert('You can use numbers lower than 1...')
+    }
+    else if (row <= 70) {
+        for (let i = 0; i < row*row; i++) {
         rows = document.createElement('div');
         rows.setAttribute('class', 'square');
         rows.style.width = (850/row) + 'px';
         rows.style.height = (850/row) + 'px';
         board.appendChild(rows);
+        };
     }
 }
 
